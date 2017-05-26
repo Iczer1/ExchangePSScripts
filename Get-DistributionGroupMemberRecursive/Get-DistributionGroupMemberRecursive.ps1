64680306121cfd85040849438708ecc6a9a2b61a
Iczer1 <john.mello@outlook.com>
2017-05-25 22:26:33 -0400

    Adding function

---
+++ Get-DistributionGroupMemberRecursive/Get-DistributionGroupMemberRecursive.ps1
@@ -0,0 +1,53 @@
+<#
+.SYNOPSIS
+Returns a recursive list of distribution group members along with an two extra properties detailing the SourceGroup name and the $N
+.DESCRIPTION
+
+.PARAMETER identity
+
+.PARAMETER MailObjectsOnly
+
+.EXAMPLE
+
+.EXAMPLE
+
+#>
+Function Get-DistributionGroupMemberRecursive {
+
+    [CmdletBinding()]
+    Param(    
+        [Parameter(Mandatory=$TRUE)] 
+        [string]$identity,
+
+        [Parameter()] 
+        [Switch]$MailObjectsOnly
+    )
+       Try {
+        $Members = Get-DistributionGroupMember -identity $identity -ResultSize Unlimited -ErrorAction stop
+        if ($MailObjectsOnly) {
+            $Members = $Members |
+                Where-Object PrimarySmtpAddress
+        }#if ($MailObjectsOnly) 
+    }
+    Catch [System.Management.Automation.CommandNotFoundException] {
+        Write-warning "Command not found, are the Exchange cmdelts loaded?"
+        Exit 1
+    }#Catch
+    Catch{
+        Write-Warning "Issue getting group info, details"
+        $_
+        Exit 1
+   }#Catch
+    [Array]$NestedPath += $identity
+
+    $Members | 
+        ForEach-Object {
+            $_ | 
+                Add-Member -MemberType NoteProperty -Name SourceGroup -Value $identity -PassThru |
+                Add-Member -MemberType NoteProperty -Name NestedGroupPath -Value $NestedPath
+            if ($_.RecipientType -match "(MailUniversalSecurityGroup)|(MailUniversalDistributionGroup)") {
+                Get-DistributionGroupMemberRecursive -identity $_.SamAccountName
+            }#if ($_.RecipientType -match "(MailUniversalSecurityGroup)|(MailUniversalDistributionGroup)")
+        }#ForEach-Object
+    Return $Members
+}#Get-DistributionGroupMemberRecursive 
